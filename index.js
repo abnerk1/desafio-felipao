814e184158074c07f0371bbb49dca05827665f42
abnerk1 <kennedyaraujo5@gmail.com>
2024-01-02 00:07:53 -0300

    meu segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-asdfjk;
+let name = "Pikachu"
+let xp = 1000
+const messageOne = "O Heroi de nome "
+const messageTwo = " esta no nivel "
+    if (xp <=1000) {
+    console.log(messageOne + name + messageTwo + "Ferro")
+    }
+    else if (xp <=2000) {
+    console.log (messageOne + name + messageTwo + "Bronze")
+    }
+    else if (xp <=5000) {
+    console.log (messageOne + name + messageTwo + "Prata")
+    }
+    else if (xp <=7000) {
+      console.log (messageOne + name + messageTwo + "Ouro")
+    }
+    else if (xp <=8000) {
+      console.log (messageOne + name + messageTwo + "Platina")
+    }
+     else if (xp <=9000) {
+      console.log (messageOne + name + messageTwo + "Ascendente")
+    }
+      else if (xp <=10000) {
+      console.log (messageOne + name + messageTwo + "Imortal")
+    }
+     else{
+      console.log (messageOne + name + messageTwo + "Radiante")
+     }
